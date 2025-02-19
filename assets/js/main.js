6d60382bf39be16740d08c6679d86bc630dd632a
Manjur-WD <mak985812@gmail.com>
2025-02-19 09:13:15 +0530

    updated

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,32 @@
 // HEADER STICKY BACKGROUND
 const header = document.getElementById("header");
-document.onscroll = function(){
-    if(window.scrollY > header.offsetHeight){
+document.onscroll = function () {
+    if (window.scrollY > header.offsetHeight) {
         header.classList.add("animate__animated");
         header.classList.add("animate__slideInDown");
         header.classList.add("active");
     }
-    else{
+    else {
         header.classList.remove("animate__animated");
-        header.classList.remove("animate__slideInDown"); 
-        header.classList.remove("active"); 
+        header.classList.remove("animate__slideInDown");
+        header.classList.remove("active");
     }
 }
+
+document.addEventListener("DOMContentLoaded", function () {
+    var swiper = new Swiper(".mySwiper", {
+        loop: true,         // Infinite loop
+        autoplay: {
+            delay: 3000,    // Auto-slide every 3s
+            disableOnInteraction: false
+        },
+        navigation: { 
+            nextEl: ".custom-next",
+            prevEl: ".custom-prev"
+        },
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true
+        }
+    });
+});
